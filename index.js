403e6baf26bc12e8e547a3fac4c690e065f76d94
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-04-23 18:09:00 +0530

    practice rest

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 let posts=[
     {
+        id:"1a",
         username:"surajjagtap@gmail.com",
         content: "  Hello Guys I am suraj jagtap from imperial college of engineering wagholi,pune"
     },
     {
+        id:"2b",
         username:"kartikizanane04@gmail.com",
         content:"  Hello Guys I am kartiki zanzane from shree siddheshwar vidyalaya,panvel"
     },
     {
+        id:"3c",
         username:"surajjagtap@gmail.com",
         content:"  kartiki you know very well about our frienship .so lets go to baramati to meet your relatives "
     }
@@ -30,11 +33,24 @@ res.render("index.ejs",{posts});
 app.listen(port,()=>{
     console.log("app is listening on port 9836");
 })
+app.get("/posts/new",(req,res)=>{
+res.render("new.ejs");
 
+})
+app.post("/posts",(req,res)=>{
+console.log(req.body);
+ let {username,content}=req.body;
+ posts.push({username,content})
 
+res.send("working successfully...")
+})
 
+app.get("/posts/:id" ,(req,res)=>{
+ let {id}=req.params;
+ let post=posts.find((p)=>id===p.id)
+ res.render("show.ejs",{post})
 
-
+})
 
 
 
