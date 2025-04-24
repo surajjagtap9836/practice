ca963a3b9c2ea64fe5124aa5c65f36e1a9d6fd62
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-04-24 18:20:34 +0530

    learn method override cocept

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express=require("express");
 const app=express();
 let port=9836;
 const path=require("path")
+const {v4:uuidv4}=require("uuid");
+uuidv4();
+let methodOverride = require('method-override')
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -11,17 +15,17 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:"surajjagtap@gmail.com",
         content: "  Hello Guys I am suraj jagtap from imperial college of engineering wagholi,pune"
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username:"kartikizanane04@gmail.com",
         content:"  Hello Guys I am kartiki zanzane from shree siddheshwar vidyalaya,panvel"
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username:"surajjagtap@gmail.com",
         content:"  kartiki you know very well about our frienship .so lets go to baramati to meet your relatives "
     }
@@ -39,8 +43,9 @@ res.render("new.ejs");
 })
 app.post("/posts",(req,res)=>{
 console.log(req.body);
- let {username,content}=req.body;
- posts.push({username,content})
+let id=uuidv4();
+let {username,content}=req.body;
+ posts.push({id,username,content})
 
 res.send("working successfully...")
 })
@@ -50,6 +55,22 @@ app.get("/posts/:id" ,(req,res)=>{
  let post=posts.find((p)=>id===p.id)
  res.render("show.ejs",{post})
 
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id)
+    res.render("edit.ejs",{post})
+})
+
+app.patch("/posts/:id",(req,res)=>{
+
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    let newcontent=req.body.content;
+    post.content=newcontent;
+    console.log(post);
+    res.send("successfully....")
 })
 
 
